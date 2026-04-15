2b21a0f67e11254de1cec63cdd0d3020ad0499f9
gusiripavan-wq <gusiripavan@gmail.com>
2026-04-15 10:07:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+function getWeather() {
+    const city = document.getElementById("clityInput").value;
+
+    const apiKey = "your_real_api_key_here"; 
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById("weatherResult").innerHTML =
+                "Temperature: " + data.main.temp + "°C";
+        })
+        .catch(error => {
+            console.log("Error:", error);
+        });
+}
