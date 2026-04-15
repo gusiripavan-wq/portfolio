7f5415c814466eba86a00310ceaf75b024aac3c6
gusiripavan-wq <gusiripavan@gmail.com>
2026-04-15 10:54:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getWeather() {
     const city = document.getElementById("cityInput").value;
 
-    const apiKey = "YOUR_API_KEY_HERE";
+    const apiKey = "2a0d87abb91d7cafc7e1f9b4976f551e";
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
