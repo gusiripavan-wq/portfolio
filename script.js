cfec087a852c5374247c369666554827abe6d313
gusiripavan-wq <gusiripavan@gmail.com>
2026-04-15 10:09:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-
 function getWeather() {
-    const city = document.getElementById("clityInput").value;
+    const city = document.getElementById("cityInput").value;
 
-    const apiKey = "your_real_api_key_here"; 
+    const apiKey = "YOUR_API_KEY_HERE";
 
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     fetch(url)
         .then(response => response.json())
         .then(data => {
-            document.getElementById("weatherResult").innerHTML =
-                "Temperature: " + data.main.temp + "°C";
+            if (data.cod === 200) {
+                document.getElementById("weatherResult").innerHTML =
+                    "🌡 Temperature: " + data.main.temp + "°C<br>" +
+                    "🌥 Weather: " + data.weather[0].description;
+            } else {
+                document.getElementById("weatherResult").innerHTML =
+                    "❌ City not found";
+            }
         })
         .catch(error => {
             console.log("Error:", error);
